c3f0446d827bb6b3f30b7421fb8706348b0bb351
kokko.sampo@gmail.com <kokko.sampo@gmail.com>
2023-01-30 00:13:17 +0200

    Went from Lsb0 to Msb0 and fixed the getter function indices to get the correct bits. Frame header data reading seems to be working

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use mpeg_file_structure::*;
 
 const SYNC_LEN: usize = 11;
 
-type Bits = BitVec<u8, Lsb0>;
+type Bits = BitVec<u8, Msb0>;
 
 mod mpeg_file_structure {
-    use std::fmt::Display;
+    use std::fmt::{self, Display};
 
     use super::*;
 
@@ -20,23 +20,35 @@ mod mpeg_file_structure {
     //FrameLen = ((144 * BitRate / SampleRate ) + Padding) as usize
 
     pub struct Header {
-        data: Bits,
+        pub data: Bits,
+    }
+
+    impl From<Bits> for Header {
+        fn from(header_data: Bits) -> Self {
+            Header { data: header_data }
+        }
+    }
+
+    impl Header {
+        fn get_frame_len(&self) -> usize {
+            ((144 * self.get_bitrate() as usize) / self.get_sampling_rate() as usize) + self.get_padding() as usize
+        }
     }
 
     pub trait MpegHeader {
         fn get_syncword(&self) -> String;
-        fn get_version_id(&self) -> &u8;
-        fn get_layer(&self) -> &u8;
+        fn get_version_id(&self) -> u8;
+        fn get_layer(&self) -> u8;
         fn get_crcp(&self) -> bool;
-        fn get_bitrate(&self) -> &u16;
-        fn get_sampling_rate(&self) -> &u16;
+        fn get_bitrate(&self) -> u16;
+        fn get_sampling_rate(&self) -> u16;
         fn get_padding(&self) -> bool;
         fn get_private_bit(&self) -> bool;
-        fn get_channel(&self) -> &u8;
-        fn get_mode_extension(&self) -> &u8;
+        fn get_channel(&self) -> u8;
+        fn get_mode_extension(&self) -> u8;
         fn get_copyright(&self) -> bool;
         fn get_orig(&self) -> bool;
-        fn get_emphasis(&self) -> &u8;
+        fn get_emphasis(&self) -> u8;
     }
 
     impl MpegHeader for Frame {
@@ -44,11 +56,11 @@ mod mpeg_file_structure {
             self.header.get_syncword()
         }
 
-        fn get_version_id(&self) -> &u8 {
+        fn get_version_id(&self) -> u8 {
             self.header.get_version_id()
         }
 
-        fn get_layer(&self) -> &u8 {
+        fn get_layer(&self) -> u8 {
             self.header.get_layer()
         }
 
@@ -56,11 +68,11 @@ mod mpeg_file_structure {
             self.header.get_crcp()
         }
 
-        fn get_bitrate(&self) -> &u16 {
+        fn get_bitrate(&self) -> u16 {
             self.header.get_bitrate()
         }
 
-        fn get_sampling_rate(&self) -> &u16 {
+        fn get_sampling_rate(&self) -> u16 {
             self.header.get_sampling_rate()
         }
 
@@ -71,11 +83,11 @@ mod mpeg_file_structure {
         fn get_private_bit(&self) -> bool {
             self.header.get_private_bit()
         }
-        fn get_channel(&self) -> &u8 {
+        fn get_channel(&self) -> u8 {
             self.header.get_channel()
         }
 
-        fn get_mode_extension(&self) -> &u8 {
+        fn get_mode_extension(&self) -> u8 {
             self.header.get_mode_extension()
         }
 
@@ -87,7 +99,7 @@ mod mpeg_file_structure {
             self.header.get_orig()
         }
 
-        fn get_emphasis(&self) -> &u8 {
+        fn get_emphasis(&self) -> u8 {
             self.header.get_emphasis()
         }
     }
@@ -97,87 +109,149 @@ mod mpeg_file_structure {
             self.data[0..11].to_string()
         }
 
-        fn get_version_id(&self) -> &u8 {
-            &self.data[11..13].load()
+        fn get_version_id(&self) -> u8 {
+            self.data[11..13].load()
         }
 
-        fn get_layer(&self) -> &u8 {
-            &self.data[13..15].load()
+        fn get_layer(&self) -> u8 {
+            self.data[13..15].load()
         }
 
         fn get_crcp(&self) -> bool {
             self.data[15] == true
         }
 
-        fn get_bitrate(&self) -> &u16 {
-            &self.data[16..21].load()
+        fn get_bitrate(&self) -> u16 {
+            self.data[16..20].load()
         }
 
-        fn get_sampling_rate(&self) -> &u16 {
-            &self.data[21..23].load()
+        fn get_sampling_rate(&self) -> u16 {
+            self.data[20..22].load()
         }
 
         fn get_padding(&self) -> bool {
-            self.data[23] == true
+            self.data[22] == true
         }
 
         fn get_private_bit(&self) -> bool {
-            self.data[24] == true
+            self.data[23] == true
         }
 
-        fn get_channel(&self) -> &u8 {
-            &self.data[25..27].load()
+        fn get_channel(&self) -> u8 {
+            self.data[22..26].load()
         }
 
-        fn get_mode_extension(&self) -> &u8 {
-            &self.data[27..29].load()
+        fn get_mode_extension(&self) -> u8 {
+            self.data[26..28].load()
         }
 
         fn get_copyright(&self) -> bool {
-            self.data[29] == true
+            self.data[28] == true
         }
 
         fn get_orig(&self) -> bool {
-            self.data[30] == true
+            self.data[29] == true
         }
 
-        fn get_emphasis(&self) -> &u8 {
-            &self.data[31..32].load()
+        fn get_emphasis(&self) -> u8 {
+            self.data[30..32].load()
         }
     }
 
     impl Display for Header {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(f, "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
-                   self.get
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            write!(f, "MPEG version: {} layer {}\nCRCP protection: {}\nBitrate: {} kbps\nSampling rate: {} Hz\nFrame padding: {}\nPrivate bit: {}\nChannel: {}\nCopyrighted: {}\nOriginal media: {}\nEmphasis: {}",
+                   match self.get_version_id() {
+                       0 => "2.5",
+                       1 => "error",
+                       2 => "2",
+                       _ => "1",
+                   },
+                   match self.get_layer() {
+                       0 => "error",
+                       1 => "III",
+                       2 => "II",
+                       _ => "I",
+                   },
+                   match self.get_crcp() {
+                       false => "Yes",
+                       true => "No ",
+                   },
+                   match self.get_bitrate() {
+                       0 => "free",
+                       1 => "32",
+                       2 => "40",
+                       3 => "48",
+                       4 => "56",
+                       5 => "64",
+                       6 => "80",
+                       7 => "96",
+                       8 => "112",
+                       9 => "128",
+                       10 => "160",
+                       11 => "192",
+                       12 => "224",
+                       13 => "256",
+                       14 => "320",
+                       _ => {
+                           println!("{}", self.get_bitrate());
+                           "brokey"
+                       },
+                   },
+                   match self.get_sampling_rate() {
+                       0 => "44100",
+                       1 => "48000",
+                       2 => "32000",
+                       _ => "error",
+                   },
+                   match self.get_padding() {
+                       true => "yes",
+                       false => "no",
+                   },
+                   self.get_private_bit(),
+                   match self.get_channel() {
+                       0 => "Stereo".to_string(),
+                       1 => format!("Joint stereo: {}", match self.get_mode_extension() {
+                            0 => "No intensity stereo or MS stereo",
+                            1 => "Intensity stereo on, MS stereo off",
+                            2 => "Intensity stereo off, MS stereo off",
+                            _ => "Intensity stereo on, MS stereo on",
+                       }),
+                       2 => "Dual".to_string(),
+                       _ => "Mono".to_string(),
+                   },
+                   match self.get_copyright() {
+                       true => "yes",
+                       false => "no",
+                   },
+                   match self.get_orig() {
+                       true => "yes",
+                       false => "no",
+                   },
+                   match self.get_emphasis() {
+                       0 => "None",
+                       1 => "50/15",
+                       2 => "error",
+                       _ => "CCIT J.17",
+                   })
+        }
+    }
 }
 
 fn read_first_frame(path: &str) -> Header {
     let mut contents: Bits = BitVec::new();
-    let syncword: Bits = BitVec::repeat(true, 11);
+    let syncword: Bits = BitVec::repeat(true, SYNC_LEN);
     io::copy(&mut File::open(path).unwrap(), &mut contents).expect("Assuming io::copy is all gucci");
     let mut header_data: Bits = BitVec::new();
     contents.windows(32)
-        .skip_while(|x| x[..11] != syncword)
+        .skip_while(|x| {
+            println!("asd");
+            x[..11] != syncword
+        })
         .next()
         .unwrap()
         .clone_into(&mut header_data);
-    Header { data: header_data }
+    Header::from(header_data)
 }
 
 fn read_file(path: &str) {
@@ -208,5 +282,19 @@ fn read_file(path: &str) {
 fn main() {
     let path = "test.mp3";
     let frame = read_first_frame(path);
-    println!("{:?}", frame.data);
+    println!("{}", frame);
+    let mut test_field: Bits = BitVec::new();
+    io::copy(&mut File::open(path).unwrap(), &mut test_field).unwrap();
+    for i in (0..124).step_by(4) {
+        println!("{:?}", &test_field[i..i+4]);
+    }
+
+    let path = "huh.mp3";
+    let frame = read_first_frame(path);
+    println!("{}", frame);
+    let mut test_field: Bits = BitVec::new();
+    io::copy(&mut File::open(path).unwrap(), &mut test_field).unwrap();
+    for i in (0..124).step_by(4) {
+        println!("{:?}", &test_field[i..i+4]);
+    }
 }
